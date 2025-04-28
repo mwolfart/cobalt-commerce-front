5a4531254948fbb44a7cbc36f7840691300051da
Murilo Wolfart <murilo.wolfart@materialplus.io>
2025-04-28 18:19:08 -0300

    chore: remove font & use next fonts; add nav

--- src/components/feature/header/header.tsx
+++ src/components/feature/header/header.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between w-full px-8 py-4 bg-sky-100">
-      <Image src="/logo.png" alt="Cobalt Shop" width={128} height={24} />
+    <header className="flex items-center justify-between w-full px-8 py-4 bg-gray-100">
+      {/* <Image src="/logo.png" alt="Cobalt Shop" width={128} height={24} /> */}
+      <span className="text-sky-400 font-zain text-3xl">Cobalt Shop</span>
     </header>
   );
 };
---
+++ src/components/feature/nav/nav.tsx
@@ -0,0 +1,35 @@
+type Props = {
+  items: {
+    title: string;
+    subitems?: string[];
+  }[];
+};
+
+export const Nav = ({ items }: Props) => {
+  return (
+    <nav className="flex items-center justify-center w-full bg-blue-400">
+      <ul className="flex items-center justify-center bg-white gap-[1px]">
+        {items.map(({ title, subitems }, index) => (
+          <li key={index} className="relative bg-blue-400 group px-4 py-2">
+            <a className="text-white font-semibold text-lg" href="#">
+              {title}
+            </a>
+            {subitems && subitems.length > 0 && (
+              <div className="absolute top-[44px] left-0 right-0 h-fit bg-white shadow-lg p-4 hidden group-hover:block">
+                <ul className="flex flex-col gap-2">
+                  {subitems?.map((subitem, subindex) => (
+                    <li key={subindex} className="">
+                      <a className="font-semibold text-sky-800" href="#">
+                        {subitem}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
